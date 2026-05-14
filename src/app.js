2f17b7700d5412f5c20f1b31f6a5cc0e97e32265
Adrian Castro <laughingmandev@gmail.com>
2026-05-14 08:04:42 +0000

    Generator v2

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.generarHack = function() {
+    let inpuText = document.getElementById("word").value.replace(/\s+/g, '');
+    let display = document.getElementById("result");
+
+    if (inpuText.length < 4) {
+        display.innerText = "Esta bien que no quieras usar el cerebro... pero no abuses!, coloca almenos cuatro letritas";
+    } else {
+        let nombre = inpuText.slice(0, -3);
+        let tld = inpuText.slice(-3);
+        
+        let hack = nombre + "." + tld;
+        
+        display.innerText = "Tu dominio sera: " + hack.toLowerCase();
+    }
+}
