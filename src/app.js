2af39c70137791ba3e5fe16cac8b583059c81e19
Adrian Castro <laughingmandev@gmail.com>
2026-05-16 02:02:27 +0000

    Simplificando Elementos Guardando Codigos Anteriores

--- src/app.js
+++ src/app.js
@@ -22,3 +22,19 @@ window.genDomain = function() {
     
 }
 
+//******Codigo anterior********
+//     window.generarHack = function() {
+//     let inpuText = document.getElementById("word").value.replace(/\s+/g, '');
+//     let display = document.getElementById("result");
+
+//     if (inpuText.length < 4) {
+//         display.innerText = "Esta bien que no quieras usar el cerebro... pero no abuses!, coloca almenos cuatro letritas";
+//     } else {
+//         let nombre = inpuText.slice(0, -3);
+//         let tld = inpuText.slice(-3);
+        
+//         let hack = nombre + "." + tld;
+        
+//         display.innerText = "Tu dominio sera: " + hack.toLowerCase();
+//     }
+// }
